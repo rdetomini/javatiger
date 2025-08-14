129af1352487c0b51b5c1ff8dce1a06b233ff216
Rodrigo Detomini <rdetomini@hotmail.com>
2025-08-14 15:24:03 -0300

    Criação projeto

---
+++ jatiger/src/main/java/com/jagrinho/jatiger/JatigerApplication.java
@@ -0,0 +1,13 @@
+package com.jagrinho.jatiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JatigerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JatigerApplication.class, args);
+	}
+
+}
---
+++ jatiger/src/test/java/com/jagrinho/jatiger/JatigerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jagrinho.jatiger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JatigerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
