33f62ca67e67ed7c41d118c370d89249a5ae4fc9
Rodrigo Detomini <rdetomini@hotmail.com>
2025-08-14 20:28:07 -0300

    Criação repositório

---
+++ jatiger/src/main/java/com/jagrinho/jatiger/domain/repositories/EmpresaRepository.java
@@ -0,0 +1,11 @@
+package com.jagrinho.jatiger.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jagrinho.jatiger.domain.entities.Empresa;
+
+@Repository
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+
+}
